ace952ffa8cfd5642da6123cc490bfadac9b0055
Elliot <76738069+Ellicode@users.noreply.github.com>
2024-01-28 19:58:43 -0500

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,3 @@
+
+// The toolbox gets passed to the configuration struct during injection.
+const workspace = Blockly.inject("blocklyDiv", { toolbox: TOOLBOX }); //, renderer: "zelos", theme: Blockly.Themes.Zelos
